263439f6f88ab99f2835fe575e92218b845e416f
moustajil.dev@gmail.com <moustajil.dev@gmail.com>
2025-12-04 12:21:06 +0100

    add random roll for two users

--- script.js
+++ script.js
@@ -26,16 +26,38 @@ const user1RollBtn = document.querySelector(".btn-play-1");
 const user2RollBtn = document.querySelector(".btn-play-2");
 const rollDiceSauqre = document.querySelector(".roll-dice");
 
+let turn = "player1";
 
+// Initial state
+user1RollBtn.disabled = false;
+user2RollBtn.disabled = true;
 
 user1RollBtn.addEventListener("click", function () {
-    var randomRolldice = Math.floor(Math.random() * 6) + 1;
+    if (turn === "player1") {
+        let randomRolldice = Math.floor(Math.random() * 6) + 1;
+        rollDiceSauqre.textContent = randomRolldice;
 
-    rollDiceSauqre.textContent = randomRolldice;
+        turn = "player2";
+
+        user1RollBtn.disabled = true;
+        user2RollBtn.disabled = false;
+
+        user1RollBtn.style.color = "gray";
+        user2RollBtn.style.color = "green";
+    }
 });
 
 user2RollBtn.addEventListener("click", function () {
-    var randomRolldice = Math.floor(Math.random() * 6) + 1;
+    if (turn === "player2") {
+        let randomRolldice = Math.floor(Math.random() * 6) + 1;
+        rollDiceSauqre.textContent = randomRolldice;
 
-    rollDiceSauqre.textContent = randomRolldice;
+        turn = "player1";
+
+        user2RollBtn.disabled = true;
+        user1RollBtn.disabled = false;
+
+        user2RollBtn.style.color = "gray";
+        user1RollBtn.style.color = "green";
+    }
 });
